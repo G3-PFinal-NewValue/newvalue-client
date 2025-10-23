904fe8794b4e50e6caeaeabb28ec7033ffa6dbce
luisasilva99 <luisasilvamartinez1999@gmail.com>
2025-10-23 10:52:26 +0000

    added category filter in blog page

--- src/pages/public/Blog/BlogListPage.jsx
+++ src/pages/public/Blog/BlogListPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import BlogCard from "../../../components/BlogCard";
 import "./blog.css";
 
@@ -29,20 +30,47 @@ const articles = [
   // Más artículos...
 ];
 
+const categories = ["Todos", "Mindfulness", "Estrés", "Terapia", "Salud Mental", "Bienestar", "Ansiedad", "Comunicación"];
+
 export default function BlogCardList() {
+  const [selectedCategory, setSelectedCategory] = useState("Todos");
+
+  const filteredArticles =
+    selectedCategory === "Todos"
+      ? articles
+      : articles.filter((a) => a.label.toLowerCase() === selectedCategory.toLowerCase());
+
   return (
-    <div className="blogcard-list">
-      {articles.map((a, i) => (
-        <BlogCard
-          key={i}
-          image={a.image}
-          label={a.label}
-          title={a.title}
-          description={a.description}
-          author={a.author}
-          slug={a.slug}
-        />
-      ))}
+    <div className="blog-page">
+      <div className="blog-sidebar">
+        <h3>Categorías</h3>
+        <ul>
+          {categories.map((cat, i) => (
+            <li
+              key={i}
+              className={selectedCategory === cat ? "active" : ""}
+              onClick={() => setSelectedCategory(cat)}
+            >
+              {cat}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Lista de artículos */}
+      <div className="blogcard-list">
+        {filteredArticles.map((a, i) => (
+          <BlogCard
+            key={i}
+            image={a.image}
+            label={a.label}
+            title={a.title}
+            description={a.description}
+            author={a.author}
+            slug={a.slug}
+          />
+        ))}
+      </div>
     </div>
   );
 }
